9504d6eb0c0dd554ce0e71431011c5f3db334b6b
ShafiArnob <shafiarnob@gmail.com>
2023-04-28 01:35:42 +0600

    sidebar tailwind

--- drawer-maker/src/App.js
+++ drawer-maker/src/App.js
@@ -17,15 +17,16 @@ function App() {
   return (
     <div className="App">
 
-      <div className={` ${click? 'style-after-drawer' : 'style-before-drawer'}`} id="overlay" onClick={pageclick}></div>
+      <div className={` ${click? 'fixed w-full h-full top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80 z-10 cursor-pointer transition-all duration-500 ease-out' : 'bg-transparent'}`} id="overlay" onClick={pageclick}></div>
 
-      <div className='page-container'>
+      <div className='relative min-w-[100vw]'>
       <button onClick={clickHandler}>Open</button>
-        <div className={`main-page`} onClick={pageclick}>
+        <div className='min-h-[100vh]' onClick={pageclick}>
           <h1 className='text-red-900'>This is the main page</h1>
         </div>
-        <div className={`drawer-container ${click? 'drawer-open' : 'drawer-close'}`}>
-          <h1>This is the drawer</h1>
+        {/* sidebar container */}
+        <div className={`${click? 'right-0' : '-right-52'} fixed top-0 text-white h-[100vh] bg-black   transition-all duration-500 ease-out z-30 `}>
+          <h1>This is the drawer hello</h1>
         </div>
       </div>
     </div>
