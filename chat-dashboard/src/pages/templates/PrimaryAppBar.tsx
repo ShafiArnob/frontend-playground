1af3aa766d31cd05b73e3cea2c9ba15487a0ce68
ShafiArnob <shafiarnob@gmail.com>
2023-07-10 18:14:34 +0600

    sidebar and responsive navbar

--- chat-dashboard/src/pages/templates/PrimaryAppBar.tsx
+++ chat-dashboard/src/pages/templates/PrimaryAppBar.tsx
@@ -1,12 +1,27 @@
-import {AppBar, Toolbar, Link, Typography} from '@mui/material'
+import {AppBar, Toolbar, Link, Typography, Box, IconButton, Drawer, useMediaQuery} from '@mui/material'
 import { useTheme, responsiveFontSizes } from '@mui/material/styles'
+import MenuIcon from "@mui/icons-material/Menu"
+import React, { useEffect, useState } from 'react'
 // import { Link } from 'react-router-dom'
 
 const PrimaryAppBar = () => {
+  const [sideMenu, setSideMenu] = useState(false)
   const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"))
+
+  useEffect(()=>{
+    if(isSmallScreen && sideMenu){
+      setSideMenu(false)
+    }
+  }, [isSmallScreen])
+
+  const toggleDrawer = (open:boolean) => (event:React.MouseEvent)=>{
+    setSideMenu(open)
+  }
   return (
     <AppBar 
       sx={{
+        zIndex: (theme)=> theme.zIndex.drawer + 2,
         backgroundColor:theme.palette.background.default,
         borderBottom:`1px solid ${theme.palette.divider}`
       }}>
@@ -16,6 +31,18 @@ const PrimaryAppBar = () => {
           height:theme.primaryAppBar.height, 
           minHeight:theme.primaryAppBar.height
       }}>
+        <Box sx={{display:{xs:"block", sm:"none"}}}>
+          <IconButton color='inherit' aria-label='open drawer' edge="start" onClick={toggleDrawer(true)} sx={{mr:2}}>
+            <MenuIcon/>
+          </IconButton>
+        </Box>
+
+        <Drawer anchor='left' open={sideMenu} onClose={toggleDrawer(false)}>
+          {[...Array(100)].map((_, i)=>(
+            <Typography key={i}>{i}</Typography>
+          ))}
+        </Drawer>
+
         <Link href='/' underline='none' color='inherit'>
           <Typography variant='h6' noWrap component="div" sx={{display:{fontWeight:700,letterSpacing:'-0.5px'}}}>
             DJCHAT
