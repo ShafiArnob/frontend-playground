219dfa37b650666777c3fad1e75e52a6e44f293d
ShafiArnob <shafiarnob@gmail.com>
2023-07-10 17:29:25 +0600

    responsive font sizes

--- chat-dashboard/src/pages/templates/PrimaryAppBar.tsx
+++ chat-dashboard/src/pages/templates/PrimaryAppBar.tsx
@@ -1,5 +1,5 @@
 import {AppBar, Toolbar, Link, Typography} from '@mui/material'
-import { useTheme } from '@mui/material/styles'
+import { useTheme, responsiveFontSizes } from '@mui/material/styles'
 // import { Link } from 'react-router-dom'
 
 const PrimaryAppBar = () => {
--- chat-dashboard/src/theme/theme.tsx
+++ chat-dashboard/src/theme/theme.tsx
@@ -1,4 +1,4 @@
-import {createTheme} from "@mui/material"
+import {createTheme, responsiveFontSizes} from "@mui/material"
 
 declare module "@mui/material/styles" {
   interface Theme {
@@ -15,7 +15,7 @@ declare module "@mui/material/styles" {
 }
 
 export const createMuiTheme = () => {
-  const theme = createTheme({
+  let theme = createTheme({
     typography:{
       fontFamily:['IBM Plex Sans', 'sans-serif'].join(",")
     },
@@ -31,6 +31,7 @@ export const createMuiTheme = () => {
       }
     }
   })
+  theme = responsiveFontSizes(theme)
   return theme
 }
 
