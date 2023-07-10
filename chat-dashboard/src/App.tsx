97ce15a50639c43f1752303134ade9c218ba1d14
ShafiArnob <shafiarnob@gmail.com>
2023-07-10 16:13:15 +0600

    reinitialize chat dashboard to TS

---
+++ chat-dashboard/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Home from './pages/Home'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<Home/>}/>
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
