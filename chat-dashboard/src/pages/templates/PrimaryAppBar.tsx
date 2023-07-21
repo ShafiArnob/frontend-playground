27a14cf372f49b18ce5a27e2b11d346d1126a80c
ShafiArnob <shafiarnob@gmail.com>
2023-07-21 16:28:26 +0600

    primary appbar show categories

--- chat-dashboard/src/pages/templates/PrimaryAppBar.tsx
+++ chat-dashboard/src/pages/templates/PrimaryAppBar.tsx
@@ -2,6 +2,9 @@ import {AppBar, Toolbar, Link, Typography, Box, IconButton, Drawer, useMediaQuer
 import { useTheme, responsiveFontSizes } from '@mui/material/styles'
 import MenuIcon from "@mui/icons-material/Menu"
 import React, { useEffect, useState } from 'react'
+import ExploreCategories from '../../components/SecondaryDraw/ExploreCategories'
+import AccountButton from "../../components/PrimaryAppBar/AccountButton";
+
 // import { Link } from 'react-router-dom'
 
 const PrimaryAppBar = () => {
@@ -15,40 +18,67 @@ const PrimaryAppBar = () => {
     }
   }, [isSmallScreen])
 
-  const toggleDrawer = (open:boolean) => (event:React.MouseEvent)=>{
-    setSideMenu(open)
-  }
-  
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+      setSideMenu(open);
+    };
+
+  const list = () => (
+    <Box sx={{ paddingTop: `${theme.primaryAppBar.height}px`, minWidth: 200 }} onClick={toggleDrawer(false)}  onKeyDown={toggleDrawer(false)}>
+      <ExploreCategories />
+    </Box>
+  );
+
   return (
-    <AppBar 
+    <AppBar
       sx={{
-        zIndex: (theme)=> theme.zIndex.drawer + 2,
-        backgroundColor:theme.palette.background.default,
-        borderBottom:`1px solid ${theme.palette.divider}`
-      }}>
-      <Toolbar 
-        variant='dense' 
+        zIndex: (theme) => theme.zIndex.drawer + 2,
+        backgroundColor: theme.palette.background.default,
+        borderBottom: `1px solid ${theme.palette.divider}`,
+      }}
+    >
+      <Toolbar
+        variant="dense"
         sx={{
-          height:theme.primaryAppBar.height, 
-          minHeight:theme.primaryAppBar.height
-      }}>
-        <Box sx={{display:{xs:"block", sm:"none"}}}>
-          <IconButton color='inherit' aria-label='open drawer' edge="start" onClick={toggleDrawer(true)} sx={{mr:2}}>
-            <MenuIcon/>
+          height: theme.primaryAppBar.height,
+          minHeight: theme.primaryAppBar.height,
+        }}
+      >
+        <Box sx={{ display: { xs: "block", sm: "none" } }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={toggleDrawer(true)}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
           </IconButton>
         </Box>
 
-        <Drawer anchor='left' open={sideMenu} onClose={toggleDrawer(false)}>
-          {[...Array(100)].map((_, i)=>(
-            <Typography key={i}>{i}</Typography>
-          ))}
+        <Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+          {list()}
         </Drawer>
 
-        <Link href='/' underline='none' color='inherit'>
-          <Typography variant='h6' noWrap component="div" sx={{display:{fontWeight:700,letterSpacing:'-0.5px'}}}>
+        {/* <Link href="/" underline="none" color="inherit">
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { fontWeight: 700, letterSpacing: "-0.5px" } }}
+          >
             DJCHAT
           </Typography>
         </Link>
+        <Box sx={{ flexGrow: 1 }}></Box>
+        <AccountButton /> */}
       </Toolbar>
     </AppBar>
   )
