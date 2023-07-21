9c9dafb436ddadffd4d196cc1bf2c42ee22e3a2a
ShafiArnob <shafiarnob@gmail.com>
2023-07-22 00:02:02 +0600

    server page

---
+++ chat-dashboard/src/components/Main/MessageInterface.tsx
@@ -0,0 +1,53 @@
+import useWebSocket from "react-use-websocket"
+import {useState} from "react"
+const socketUrl = "ws://127.0.0.1:8000/ws/test"
+
+
+
+const MessageInterface = () => {
+  const [newMessage, setNewMessage] = useState<string[]>([])
+  const [message, setMessage] = useState("")
+
+  const {sendJsonMessage} = useWebSocket(socketUrl, {
+    onOpen: () => {
+      console.log("Connected");
+    },
+    onClose: () => {
+      console.log("Closed");
+    },
+    onError: () => {
+      console.log("Error");
+    },
+    onMessage: (msg) =>{
+      const data = JSON.parse(msg.data)
+      setNewMessage((prev_msg)=>[...prev_msg, data.new_message])
+    }
+  })
+
+
+
+  return (
+    <div>
+      {
+        newMessage.map((msg, index)=>{
+          return (
+            <div key={index}>
+                <p>{msg}</p>
+            </div>
+          )
+        })
+      }
+      <form action="">
+        <label>
+          Enter Message:
+          <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)}/>
+        </label>
+      </form>
+      <button
+        onClick={()=>sendJsonMessage({type:"message", message})}
+      >Send</button>
+    </div>
+  )
+}
+
+export default MessageInterface
