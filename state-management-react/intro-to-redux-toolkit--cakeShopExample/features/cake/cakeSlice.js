86f695b68dba7299027dcae88c08f973660a5f89
ShafiArnob <shafiarnob@gmail.com>
2023-11-09 20:03:56 +0600

    created slice for cake

---
+++ state-management-react/intro-to-redux-toolkit--cakeShopExample/features/cake/cakeSlice.js
@@ -0,0 +1,30 @@
+const createSlice = require("@reduxjs/toolkit").createSlice
+
+const initialState = {
+  numOfCakes:10
+}
+
+const cakeSlice = createSlice({
+  name:"cake",
+  initialState,
+  reducers:{
+    ordered: (state)=>{
+      state.numOfCakes--
+    },
+    restocked: (state, action) => {
+      state.numOfCakes += action.payload
+    }
+  }
+})
+
+/*
+  The slice taks care of
+  - Defining a action type constent
+  - Action object and action cretor
+  - switch statements in the reducer
+  - handling immuteable update in the reducer
+*/
+
+
+module.exports = cakeSlice.reducer
+module.exports = cakeSlice.actions
