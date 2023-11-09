cdcce95464ed1ae2a06e275cf38097925f127000
ShafiArnob <shafiarnob@gmail.com>
2023-11-09 15:28:17 +0600

    prev-nextjs

--- nextjs-getting-started/app/about/layout.tsx
+++ nextjs-getting-started/app/about/layout.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import styles from './styles.module.css'
 
 export default function layout({ children }: { children: React.ReactNode }) {
+  // console.log(styles);
+  
   return (
     <>
       <nav>About navbar</nav>
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
     </>
   );
 }
