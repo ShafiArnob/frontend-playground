f4f3fe848f1984f34af9e14d6d006aad6b9ee0b5
ShafiArnob <shafiarnob@gmail.com>
2023-11-09 17:04:35 +0600

    essence of actions, reducers, store

--- state-management-react/intro-to-redux--cakeShopExample/index.js
+++ state-management-react/intro-to-redux--cakeShopExample/index.js
@@ -1,1 +1,44 @@
-console.log("Hello World");
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+//action
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+// readucers
+//(prevState, action) => newState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+//store
+const store = createStore(reducer);
+console.log("Initial State: ", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Update State: ", store.getState())
+);
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
