f9381a4882a0fb0736c500ba274f3174ae0ad18e
ShafiArnob <shafiarnob@gmail.com>
2023-11-13 14:51:23 +0600

    react redux example project

--- state-management-react/react-rtk-getting-started/src/App.jsx
+++ state-management-react/react-rtk-getting-started/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import Counter from "./features/counter/Counter";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main>
-      <Counter />
+    <main className="App">
+      <AddPostForm />
+      <PostsList />
     </main>
   );
 }
