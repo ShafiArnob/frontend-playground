9a4486d6e5cfa74ae5ecba8f2744e93d44a45e1a
ShafiArnob <shafiarnob@gmail.com>
2024-02-08 14:56:42 +0600

    counterslice

--- state-management-react/redux-toolkit-getting-started/src/App.tsx
+++ state-management-react/redux-toolkit-getting-started/src/App.tsx
@@ -1,7 +1,37 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+
 function App() {
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
   return (
     <>
-      <h1>Hello</h1>
+      <div className="flex flex-row space-x-4">
+        <button
+          onClick={() => dispatch(decrement())}
+          className="border border-black p-2"
+        >
+          -
+        </button>
+        <p>{count}</p>
+        <button
+          onClick={() => dispatch(increment())}
+          className="border border-black p-2"
+        >
+          +
+        </button>
+        <button
+          onClick={() => dispatch(incrementByAmount(10))}
+          className="border border-black p-2"
+        >
+          +10
+        </button>
+      </div>
     </>
   );
 }
