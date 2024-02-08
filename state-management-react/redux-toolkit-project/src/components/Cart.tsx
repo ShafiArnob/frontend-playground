ff07a7fe081bc6f1acf0626ae0cda53fe3653935
ShafiArnob <shafiarnob@gmail.com>
2024-02-08 18:31:25 +0600

    remove from cart

--- state-management-react/redux-toolkit-project/src/components/Cart.tsx
+++ state-management-react/redux-toolkit-project/src/components/Cart.tsx
@@ -13,14 +13,13 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 import { IProduct } from '@/types/globalTypes';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import { addToCart, removeFromCart } from '@/redux/features/cartSlice';
 
 export default function Cart() {
-  //! Dummy data
-
-  const products: IProduct[] = [];
+  const { products } = useAppSelector((state) => state.cart);
+  const dispatch = useAppDispatch();
   const total = 0;
-
-  //! **
 
   return (
     <Sheet>
@@ -52,13 +51,14 @@ export default function Cart() {
                 </p>
               </div>
               <div className="border-l pl-5 flex flex-col justify-between">
-                <Button>
+                <Button onClick={() => dispatch(addToCart(product))}>
                   <HiOutlinePlus size="20" />
                 </Button>
                 <Button>
                   <HiMinus size="20" />
                 </Button>
                 <Button
+                  onClick={() => dispatch(removeFromCart(product))}
                   variant="destructive"
                   className="bg-red-500 hover:bg-red-400"
                 >
