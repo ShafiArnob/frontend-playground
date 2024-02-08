779c19554b94f887c41e024fccfd3e0f6d25955d
ShafiArnob <shafiarnob@gmail.com>
2024-02-09 05:32:16 +0600

    total price calculation in cart

--- state-management-react/redux-toolkit-project/src/components/Cart.tsx
+++ state-management-react/redux-toolkit-project/src/components/Cart.tsx
@@ -21,9 +21,9 @@ import {
 } from '@/redux/features/cartSlice';
 
 export default function Cart() {
-  const { products } = useAppSelector((state) => state.cart);
+  const { products, total } = useAppSelector((state) => state.cart);
   const dispatch = useAppDispatch();
-  const total = 0;
+  // const total = 0;
 
   return (
     <Sheet>
