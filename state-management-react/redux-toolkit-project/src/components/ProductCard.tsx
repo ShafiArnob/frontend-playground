266a6211f6c62c3d5054762dbdd94810c2a84cac
ShafiArnob <shafiarnob@gmail.com>
2024-02-09 06:50:40 +0600

    product state management

--- state-management-react/redux-toolkit-project/src/components/ProductCard.tsx
+++ state-management-react/redux-toolkit-project/src/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import { toast } from './ui/use-toast';
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
 import { useAppDispatch } from '@/redux/hook';
-import { addToCart } from '@/redux/features/cartSlice';
+import { addToCart } from '@/redux/features/cart/cartSlice';
 
 interface IProps {
   product: IProduct;
